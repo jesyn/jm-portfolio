1d51fdc63416ec88ecffd1410ce8d6e5b7ddecb1
Jesica Muñoz <jesydynathos@gmail.com>
2023-05-09 16:06:14 -0300

    fix: education module style

--- src/modules/Education/Education.tsx
+++ src/modules/Education/Education.tsx
@@ -4,13 +4,13 @@ import * as styles from './Education.module.scss';
 const Education = () => {
     return (
         <article className={styles.educationContainer}>
+            <h2 className={styles.title}> formación </h2>
             <h3> CERTIFICATE TECH DEVELOPER </h3>
             <h4> DIGITAL HOUSE</h4>
             <p> Jun 2021 - Dic 2022 </p>
-            <h3> FRONTEND DEVELOPER </h3>
+            <h3> FRONTEND SPECIALIST </h3>
             <h4> DIGITAL HOUSE</h4>
             <p> Feb 2023 - Dic 2023</p>
-            <h2 className={styles.title}> formación </h2>
         </article>
     );
 };
