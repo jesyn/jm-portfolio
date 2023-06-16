b5adff2a9431e2cd2bfba630e3234f62101ebfbc
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-16 15:28:43 -0300

    feat (contact): adding responsive styles

--- src/modules/Contact/Contact.tsx
+++ src/modules/Contact/Contact.tsx
@@ -3,15 +3,18 @@ import * as styles from './Contact.module.scss';
 
 const Contact = () => {
     return (
-        <article className={styles.contactContainer}>
+        <article className={styles.container}>
             <h2 className={styles.title}> contacto </h2>
-            <h3>
-                ¡Contáctame para que colaboremos juntos en tu próximo proyecto!
-            </h3>
-            <div className={styles.contactDetails}>
-                <p> jesica.munoz.avigliano@gmail.com</p>
-                <p> +541130474811</p>
-            </div>
+            <section className={styles.contactContainer}>
+                <h3>
+                    ¡Contáctame para que colaboremos juntos en tu próximo
+                    proyecto!
+                </h3>
+                <div className={styles.contactDetails}>
+                    <p> jesica.munoz.avigliano@gmail.com</p>
+                    <p> +541130474811</p>
+                </div>
+            </section>
         </article>
     );
 };
