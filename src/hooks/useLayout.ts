cacfc58df4923a597b72a254e640813547dc6654
Jesica Muñoz <jesydynathos@gmail.com>
2023-06-16 15:32:04 -0300

    feat (useLayout): adding layout height calc

--- src/hooks/useLayout.ts
+++ src/hooks/useLayout.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useLayout = () => {
-    const [height, setHeight] = useState(0);
+    const [height, setHeight] = useState('0');
 
     const getHeight = () => {
         const windowHeight = window.innerHeight;
@@ -10,10 +10,10 @@ const useLayout = () => {
         const footerHeight =
             document.getElementById('footer')?.offsetHeight || 0;
 
-        const resultHeight = windowHeight - navbarHeight - footerHeight;
-        setHeight(resultHeight);
+        //const resultHeight = windowHeight - navbarHeight - footerHeight;
+        const resultHeight = `calc(100vh - ${navbarHeight}px - ${footerHeight}px)`;
+        setHeight(`${resultHeight}`);
     };
-
     useEffect(() => {
         getHeight();
     }, []);
