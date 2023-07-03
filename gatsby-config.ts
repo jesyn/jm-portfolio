a7acb1bd3b3f2b8e7739217d745bc21eb49e8cf4
Jesica Muñoz <jesydynathos@gmail.com>
2023-07-03 18:01:23 -0300

    fix gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,14 +17,14 @@ const config: GatsbyConfig = {
         'gatsby-plugin-image',
         'gatsby-plugin-sharp',
         'gatsby-transformer-sharp',
-        {
+        /* {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'images',
                 path: './src/images/',
             },
             __key: 'images',
-        },
+        }, */
         {
             resolve: `gatsby-plugin-sass`,
         },
