cf051e09057b8b1b3a5524e7bbe273963788fae2
Jesica Muñoz <jesydynathos@gmail.com>
2023-10-04 17:41:56 -0300

    changing web icon, title and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,19 @@ import './../styles/_global.scss';
 import type { HeadFC, PageProps } from 'gatsby';
 import Layout from '../components/Layout/Layout';
 import { Home } from '../components/Home/Home';
+import { Helmet } from 'react-helmet';
 
 const IndexPage: React.FC<PageProps> = () => {
     return (
         <main>
+            <Helmet>
+                <title>Porfolio web Jesica Muñoz</title>
+                <meta
+                    name='description'
+                    content='Sitio web personal utilizando Gatsby, donde podrás conocer en detalle mi información personal, descargar mi CV y explorar mi experiencia profesional. Descubre quién soy, mi trayectoria y habilidades en esta página web personalizada.'
+                />
+                <link rel='icon' href='/logo.png' />
+            </Helmet>
             <Layout>
                 <Home />
             </Layout>
@@ -16,4 +25,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Porfolio web Jesica Muñoz</title>;
